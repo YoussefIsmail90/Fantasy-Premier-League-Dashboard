4768d5ac795b4bf6d645964b06b720b1d35b5fae
Youssef Ismail <92085481+YoussefIsmail90@users.noreply.github.com>
2024-12-29 06:43:07 +0200

    Update fpl_dashboard.py

--- fpl_dashboard.py
+++ fpl_dashboard.py
@@ -625,7 +625,7 @@ def tab_best_xi(players_df, difficulty_df):
     players_df["score_for_best_xi"] = (
         players_df["total_points"] 
         + 1.5 * players_df["form"] 
-        + 3.0 * players_df["club_next_difficulty"]
+        + 3.0 * ( (5-players_df["club_next_difficulty"])
     )
 
     # Function to pick top N players per position
