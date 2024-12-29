cdd9f696709759133d197f6f678d4b8d6a552785
Youssef Ismail <92085481+YoussefIsmail90@users.noreply.github.com>
2024-12-29 08:05:29 +0200

    Update fpl_dashboard.py

--- fpl_dashboard.py
+++ fpl_dashboard.py
@@ -664,6 +664,7 @@ def tab_best_xi(players_df, difficulty_df):
         '1-3-5-1': {'Goalkeeper':1, 'Defender':3, 'Midfielder':5, 'Forward':1},
         '1-4-4-2': {'Goalkeeper':1, 'Defender':4, 'Midfielder':4, 'Forward':2},
         '1-5-3-1': {'Goalkeeper':1, 'Defender':5, 'Midfielder':3, 'Forward':1},
+        '1-4-5-1': {'Goalkeeper':1, 'Defender':4, 'Midfielder':5, 'Forward':1},
         # Add more formations as needed
     }
 
