4d9fdd88b41e961d108c4075487e831edf169617
Youssef Ismail <92085481+YoussefIsmail90@users.noreply.github.com>
2025-01-02 20:14:22 +0200

    Update fpl_dashboard.py

--- fpl_dashboard.py
+++ fpl_dashboard.py
@@ -718,7 +718,7 @@ def tab_best_xi(players_df, difficulty_df):
             'Midfielder': {
                 3: [20, 50, 80],
                 4: [15, 40, 60, 85],
-                5: [10, 25, 50, 75, 90]
+                5: [10, 25, 40, 55, 80]
             },
             'Forward': {
                 1: [50],
